64ba999867f94b6e8c4f2a2d966c2f567a6814f3
unknown <khalidabdi3101@gmail.com>
2024-10-05 04:48:23 +0300

    Done

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Good Luck 💪🏾
+function reverseWords(sentence) {
+   
+    const words = sentence.split(' ');
+    
+   
+    const reversedWords = words.reverse();
+    
+   
+    return reversedWords.join(' ');
+}
+
+
+console.log(reverseWords("fikrcamp bootcamp")); 
+console.log(reverseWords("hello there!")); 
+console.log(reverseWords("welcome to my city")); 
